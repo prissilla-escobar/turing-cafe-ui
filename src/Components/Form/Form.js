a796669e0e24f2635a7d58cea34be3d83e4a6585
Prissilla Escobar <prissilla988@gmail.com>
2023-09-11 12:11:26 -0600

    Attempt to add functionality for inputs

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,29 @@
 import '../Form/Form.css'
+import { useState } from 'react'
 
-function Form() {
+function Form({ newReservation }) {
+    const [nameInput, setNameInput] = useState()
+    const [dateInput, setDateInput] = useState()
+    const [timeInput, setTimeInput] = useState()
+
+    const name = (event) => {
+        setNameInput(event.target.value)
+    }
+
+    const date = (event) => {
+        setDateInput(event.target.value)
+    }
+
+    const time = (event) => {
+        setTimeInput(event.target.value)
+    }
 
     return (
         <form>
-            <input type='text' placeholder='Name' required></input>
-            <input type='date' placeholder='Date (mm/dd)' required></input>
-            <input type='time' placeholder='Time' required></input>
-            <button>Make Reservation</button>
+            <input type='text' placeholder='Name' value={nameInput} onChange={name} required></input>
+            <input type='date' placeholder='Date (mm/dd)' value={dateInput} onChange={date} required></input>
+            <input type='time' placeholder='Time' value={timeInput} onChange={time} required></input>
+            <button onClick={() => {newReservation()}}>Make Reservation</button>
         </form>
     )
 }
